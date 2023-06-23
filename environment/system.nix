59b48b5c7e4464af23f0b26e121084c33d4634f6
Alan Walker <kanonbolt128@gmail.com>
2023-06-23 18:00:29 -0500

    fix: syntax error

--- environment/system.nix
+++ environment/system.nix
@@ -2,6 +2,6 @@
 # WARNING: Do not leave values as null or home manager will throw error or break
 {
   # home manager needs to know your user and the which path to manage
-  username: null;
-  homeDirectory: null;
+  username = null;
+  homeDirectory = null;
 }
