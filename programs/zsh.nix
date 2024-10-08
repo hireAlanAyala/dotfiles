d04162f67df1060aa149ffbb94fc46bcd7db373d
Alan Walker <kanonbolt128@gmail.com>
2024-10-08 15:50:31 -0500

    feature: made hm alias safer to use

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -13,7 +13,14 @@
       wnpx = "/mnt/c/Program\\ Files/nodejs/npx";
       wgit = "/mnt/c/Program\\ Files/nodejs/npx";
       clip = "/mnt/c/Windows/System32/clip.exe";
-      hm = "(cd ~/.config/home-manager && git add . && home-manager switch --flake ~/.config/home-manager/flake.nix#alan)";
+      hm = ''
+        cd ~/.config &&
+        git add . &&
+        home-manager switch --flake ~/.config/home-manager/flake.nix#alan &&
+        source ~/.zshrc &&
+        cd - &&
+        echo -e "''${YELLOW_ORANGE}Remember to ensure all relevant files are whitelisted in ~/.config/ .gitignore!''${RESET}"
+      '';
       ai = "node ~/documents/terminal_ai/index.js";
       fuckingInit = "sudo dockerd";
       fucking = "sudo env PATH=$PATH";
@@ -24,6 +31,11 @@
       theme = "clean";
       plugins = [ "git" "vi-mode" "web-search" ];
     };
+    initExtra = ''
+      # warning colors for hm alias echo
+      YELLOW_ORANGE='\033[38;5;214m'
+      RESET='\033[0m'
+    '';
     envExtra = ''
       if [[ -z "$NVIM" ]]; then
         # Your normal Zsh setup, including ZLE
