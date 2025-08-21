e31935c029490f4c7e945c0411179033f7a55434
Alan Walker <kanonbolt128@gmail.com>
2025-08-21 10:53:29 -0500

    refactor: cleaned up dead kickstarter code

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,3 @@
 -- Neovim Options Configuration
 -- This file sets basic editor behaviors and appearance
 
-local opt = vim.opt
-
-vim.opt.number = false -- Turn OFF line numbers
-vim.opt.relativenumber = false
-print 'OPTIONS.LUA LOADED!' -- Print message on load
-
