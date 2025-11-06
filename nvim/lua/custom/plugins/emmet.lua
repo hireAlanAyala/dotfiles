2b0d9fe49779acacd61d1fc3ec4be23bf9a61090
Alan Walker <kanonbolt128@gmail.com>
2025-11-06 11:33:32 -0600

    fix: switch from nvim-emmet to emmet-vim to avoid intrusive completion menu

--- nvim/lua/custom/plugins/emmet.lua
+++ nvim/lua/custom/plugins/emmet.lua
@@ -1,9 +1,9 @@
 return {
-  'olrtg/nvim-emmet',
+  -- do not use nvim-emmet it had instrusive completion menu options that could not be disabled
+  'mattn/emmet-vim',
+  ft = { 'html', 'css', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'markdown' },
   config = function()
-    -- Simple single-key expansion
-    vim.keymap.set({ 'i', 'n' }, '<C-e>', function()
-      require('nvim-emmet').expand_abbr()
-    end, { desc = 'Emmet expand abbreviation' })
+    -- c-y , triggers expansion
+    vim.g.emmet_mode = 'inv' -- 'i' for insert, 'n' for normal, 'v' for visual
   end,
 }
