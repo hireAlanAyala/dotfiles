b36e9bc21acd563b222cecfce88861e75a257db9
Alan Walker <kanonbolt128@gmail.com>
2025-11-06 11:31:01 -0600

    refactor: update init.lua with socket initialization, smart-notes, remove Hydrate command and sessions

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,16 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
+-- Initialize persistent socket (must be done before other modules)
+require('custom.socket').init()
+
 -- plugins I want /targets.vim
+-- firenvim
+-- nvim-dbee (does not support sqlite)
+-- explore db tuis: lazysql, rainfrog, vim-dadbod-ui, harlequin, sqlua
+-- overseer (task runner, people are using it to automate tasks they'd usually have in a separate window/pane)
+-- nvim-better-n
+-- nvim-various-textobjs (text object ideas)
 
 -- Load all modules
 require 'config.options'
@@ -11,25 +20,14 @@ require 'custom.options'
 require('config.keymaps').setup()
 require 'custom.keymaps'
 require 'custom.autocmds'
+
 require('custom.transparency').setup()
 require 'custom.commands'
 require 'custom.terminal-paste-fix'
-require('custom.sessions').setup()
--- require('custom.dtach-terminal').setup()  -- Disabled in favor of terminal-persist
 require('custom.terminal-persist').setup()
 require 'custom.terminal-input-proxy'
 require('custom.task-runner').setup()
+require('custom.smart-notes').setup()
+require 'custom.csv'
 
--- Setup plugins
 require 'custom.lazy'
-
-vim.api.nvim_create_user_command('Hydrate', function()
-  local ts = vim.fn.expand '~/.config/demo.typescript'
-  local tf = vim.fn.expand '~/.config/demo.time'
-  local cmd = string.format('hydrate-typescript %s %s', vim.fn.shellescape(ts), vim.fn.shellescape(tf))
-  if vim.bo.buftype == 'terminal' and vim.b.terminal_job_id then
-    vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. '\r')
-  else
-    vim.cmd('terminal ' .. cmd)
-  end
-end, {})
