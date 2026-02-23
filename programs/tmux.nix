e8ab9a0f3d5484ee56b9517594a32dbd9526fa30
Alan Walker <kanonbolt128@gmail.com>
2026-02-23 09:04:34 -0600

    chore: removed tmux resurrect, just open scratch on new

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -19,31 +19,6 @@
           set -g @dracula-show-edge-icons true
         '';
       }
-      {
-        plugin = resurrect;
-        extraConfig = ''
-          set -g @resurrect-capture-pane-contents 'on'
-          # Use default strategy instead of session strategy
-          # This will restore nvim with file arguments instead of session files
-          set -g @resurrect-strategy-vim 'default'
-          set -g @resurrect-strategy-nvim 'default'
-          set -g @resurrect-dir '$HOME/.tmux/resurrect'
-          
-          # Process restoration list - simplified for NixOS
-          set -g @resurrect-processes 'nvim vim ssh man less more tail top htop watch git claude'
-          
-          # NixOS-specific fixes for process restoration
-          set -g @resurrect-save-command-strategy 'ps'
-        '';
-      }
-      {
-        # INFO: must be last in tmux plugins list
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '5'
-        '';
-      }
     ];
     terminal = "tmux-256color";
     extraConfig = ''
@@ -52,10 +27,6 @@
       
       set -ga terminal-overrides ",*256col*:Tc"
       source-file ~/.config/tmux/editable-tmux.conf
-      
-      # Additional resurrect configuration for NixOS
-      set -g @resurrect-restore-processes 'nvim'
-      set -g @resurrect-processes-filter 'nvim->nvim'
     '';
   };
 }
